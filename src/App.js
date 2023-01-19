efc61e1f29dd551b250a02dabcb1d0ccf4d32fa5
Sticx <108312079+Sticx@users.noreply.github.com>
2023-01-19 21:21:50 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {Header} from "./components/Header";
+import {Todo} from "./pages/Todo";
+import {Contacts} from "./pages/Contacts";
+import {ErrorBoundary} from "./components/error/ErrorBoundery";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => (
+    <>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path={"/"} element={<Todo/>}/>
+            <Route path={"/contacts"} element={<Contacts/>}/>
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </>
+);
 
 export default App;
